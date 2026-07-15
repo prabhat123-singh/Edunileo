ea1ce6df413840312f770689a0e6a2e72123167b
prabhat123-singh <digitalmysteries123@gmail.com>
2026-07-15 13:14:56 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 // EduNileo JavaScript
+const hiddenElements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries) => {
+
+    entries.forEach((entry) => {
+
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+
+    });
+
+});
+
+hiddenElements.forEach((el)=>observer.observe(el));
